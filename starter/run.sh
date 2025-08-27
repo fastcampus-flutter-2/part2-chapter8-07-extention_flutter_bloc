99525abb5790d749c70fcc3589305074eaec4725
gasigogi <sh9306@gmail.com>
2025-08-27 17:11:11 +0900

    update flutter sdk 3.32.8

--- starter/run.sh
+++ starter/run.sh
@@ -23,7 +23,7 @@ for dir in $(find . -type f -name 'pubspec.yaml' -exec dirname "{}" \;); do
 
   if grep -q "build_runner:" pubspec.lock; then
     echo "Running build_runner to generate and build project files..."
-    $FLUTTER packages pub run build_runner build --delete-conflicting-outputs
+    $FLUTTER pub run build_runner build --delete-conflicting-outputs
   fi
 
   cd "$DIR" || exit
